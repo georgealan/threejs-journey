4bd0f3b3ee082ae74574204505fde6c859e11cbc
George Alan Rufo <georgealanrufo@gmail.com>
2023-09-09 16:26:25 -0300

    Continue in class 08. FullScreen and Resising

--- lessons/cameras/src/script.js
+++ lessons/cameras/src/script.js
@@ -1,5 +1,21 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (e) => {
+    cursor.x = - (e.clientX / sizes.width - 0.5)
+    cursor.y = e.clientY / sizes.height - 0.5
+
+    //console.log(cursor.x)
+})
 
 /**
  * Base
@@ -24,12 +40,18 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+//const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+//const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+//camera.position.x = 2
+//camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -45,7 +67,17 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    //mesh.rotation.y = elapsedTime;
+
+    // Update Camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+
+    // Update Controls
+    // If you use Damping don't forget to update at each frame
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
